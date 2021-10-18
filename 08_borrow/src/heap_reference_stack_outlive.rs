dc36369d27a9a360401846798d58a0ca07155977
Yoshi <rabbit20110410@gmail.com>
2021-10-18 13:52:49 +0800

    Changes

--- 08_borrow/src/heap_reference_stack_outlive.rs
+++ 08_borrow/src/heap_reference_stack_outlive.rs
@@ -8,5 +8,5 @@ fn main() {
 fn push_local_ref(data: &mut Vec<&u32>) {
     let v = 42;
     // v 生命周期不够长，如果注释掉会编译不过
-    // data.push(&v);
+    //data.push(&v);
 }
--- 08_borrow/src/multiple_mut_ref.rs
+++ 08_borrow/src/multiple_mut_ref.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut data = vec![1, 2, 3];
 
     // Rust 下，根据所有权规则，你无法同时拥有多个可变引用
-    // for item in data.iter_mut() {
-    //     data.push(*item + 1);
-    // }
+    for item in data.iter_mut() {
+        data.push(*item + 1);
+    }
 }
